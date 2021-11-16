e61e76c43e73e7088426390f5d1849eb499c7fa2
gabriel-arroyo <ing.gabriel.arroyo@gmail.com>
2021-11-16 01:59:12 -0600

    repair Kpi _id generator

--- src/app/shared/models/class/kpi.ts
+++ src/app/shared/models/class/kpi.ts
@@ -1,7 +1,4 @@
-import {v4 as uuidv4} from 'uuid';
-
-let myuuid = uuidv4();
-
+import { v4 as uuidv4 } from 'uuid'
 export class Kpi {
 	_id: string
 	data: string
@@ -10,12 +7,12 @@ export class Kpi {
 	options: { key: string; value: string }[]
 
 	constructor(options: {
-		_id?: string,
-		data?: string,
-		label?: string,
-		units?: string,
+		_id?: string
+		data?: string
+		label?: string
+		units?: string
 	}) {
-		this._id = options._id || myuuid
+		this._id = options._id || uuidv4()
 		this.data = options.data || '0'
 		this.label = options.label || 'Kpi'
 		this.units = options.units || ''
